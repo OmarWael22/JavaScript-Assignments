9fa78c36ac05d0314f8fb66312d3d7cda70d2ed7
OMAR WAEL <omarwael012@gmail.com>
2025-05-14 00:42:00 +0300

    Add HTML and JavaScript files for Assignment 2

---
+++ 02-DataTypes And Variables/main.js
@@ -0,0 +1,24 @@
+/* Assignment 1 */
+let numberOne = 10;
+let numberTwo = 20;
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo));
+console.log(`${numberOne}${numberTwo}`);
+console.log(`${typeof `${numberOne}${numberTwo}`}`);
+console.log(numberOne + "\n" + numberTwo);
+console.log(`${numberOne}
+${numberTwo}`);
+/* Assignment 2 */
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+/* Assignment 3 */
+console.log("`I'm In \n \
+\\\\ \n \
+Love \\\ \"\"\" ''' \n \
+++ with ++ \n \
+\\ \"\"\"\\\"\"\"");
+/*assignment 4 */
+let a = 21;
+let b = 20;
+
+console.log("_" + `${a}_${b}`.repeat(4) + "_");
